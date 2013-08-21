c87078cc222bbb464e24c9425397b656ac553b4d
Xiao G. Wu <xiaogwu@gmail.com>
2013-08-21 10:39:30 -0700

    Add Petition model and started RSpec'ing

---
+++ db/migrate/20130821171759_create_petitions.rb
@@ -0,0 +1,11 @@
+class CreatePetitions < ActiveRecord::Migration
+  def change
+    create_table :petitions do |t|
+      t.string :title
+      t.text :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/petition_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Petition do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presense_of :description }
+  it { should validate_preesnse_of :user }
+
+  it { should belong_to :user }
+  it { should have_many :signatures }
+end
