8b1823ab883fa5a574be6a237eefc6ab3954b1f9
Xiao G. Wu <xiaogwu@gmail.com>
2013-08-21 11:05:59 -0700

    Add petition associations and validations

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,3 +1,12 @@
 class Petition < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :description
+
+  has_many :signatures
+  belongs_to :user
+
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :user
+
 end
--- spec/models/petition_spec.rb
+++ spec/models/petition_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Petition do
   # pending "add some examples to (or delete) #{__FILE__}"
   it { should validate_presence_of :title }
-  it { should validate_presense_of :description }
-  it { should validate_preesnse_of :user }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :user }
 
   it { should belong_to :user }
   it { should have_many :signatures }
